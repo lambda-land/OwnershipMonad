117e4f45393f9dbc9610aa4c8da7687578566f4a
michaelmcgirr <mike@oatsmail.com>
2015-11-08 20:59:22 -0800

    Adding a more rigorous explanation to example2

--- ownership_examples/example2.rs
+++ ownership_examples/example2.rs
@@ -1,8 +1,17 @@
-// example2.rs
-// Author: mike mcgirr
-// compile with: $rustc example2.rs
+/* 
+ example2.rs
+ Author: mike mcgirr
+ compile with: $rustc example2.rs
 
+ This example is less to illustrate the ownership concept in Rust
+ and instead demonstrates the concept of Rust of Traits and their 
+ similarity to type classes in Haskell. 
 
+ The Haskell examples are taken from:
+ http://learnyouahaskell.com/making-our-own-types-and-typeclasses#typeclasses-102 
+*/
+
+// let's enumerate a data type structure and call it `TrafficLight`
 enum TrafficLight { 
                     Red,
                     Yellow,
@@ -15,12 +24,14 @@ enum TrafficLight {
                      | Green
 */
 
-// Let's create a trait called Eq
-trait Eq {
-    // Something that implemented the Eq trait would need to 
-    // have a function 
+// Let's create a trait called Eq for testing equality of ourself and one other
+// type like ourself
+trait Eq 
+{
+    // Something that implemets the Eq trait would need to have a function 
     fn is_equal(&self, TrafficLight) -> bool;
 }
+
 /* This is a lot like a type class in Haskell 
 
    class Eq a where 
@@ -29,22 +40,29 @@ trait Eq {
 */
 
 // Let's implement the Eq trait for TrafficLight
-impl Eq for TrafficLight {
+impl Eq for TrafficLight 
+{
     fn is_equal(&self, other: TrafficLight ) -> bool {
         match (self, other) 
         {
-            (&TrafficLight::Red,    TrafficLight::Red)     => true,
+            (&TrafficLight::Red,    TrafficLight::Red)     => true,  
             (&TrafficLight::Yellow, TrafficLight::Yellow)  => true,
             (&TrafficLight::Green,  TrafficLight::Green)   => true,
             _                                              => false
         }
     }
 }
-/* Imagine we couldn't derive Eq for TrafficLight automatically 
- * we had to implement it by hand.
+
+/* Imagine we couldn't derive Eq for TrafficLight using automatic syntactic sugar
+ * like the `#[derive]` attribute.
  *
- * This is sort of like writing up instance by hand in Haskell for making
- * instances of typeclasses
+ * Or that it was something that was not derived for us in the unsolicited manner that 
+ * equality is normally for some types.
+ * 
+ * In this case we have to implement it by hand.
+ *
+ * This is sort of like writing up an instance by hand in Haskell for making
+ * instances of typeclasses.
  *
  * It would be as if we did the following in Haskell
     
@@ -56,6 +74,8 @@ impl Eq for TrafficLight {
 
 */
 
+
+// Maing function for testing the above types
 fn main() 
 {
     let light : TrafficLight = TrafficLight::Red;
