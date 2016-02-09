4bb3370f74527e5ba335b47029a3bb14c440e98a
michaelmcgirr <michaelgordonmcgirr@gmail.com>
2016-02-08 17:15:16 -0800

    Tweaked the examples - experimenting with what's currently possible with crossbeam.

--- memory_profiling/copy_mem_test.rs
+++ memory_profiling/copy_mem_test.rs
@@ -6,15 +6,25 @@ fn main() {
     let data = Arc::new(Mutex::new([1, 1, 1]));
 
     for i in 0..3 {
+        // clone data
         let data_clone = data.clone();
         
         thread::spawn(move || {
             let mut mut_data = data_clone.lock().unwrap();
             
-            mut_data[i] += 1;
-            println!("element: {} is {}", i, mut_data[i]);
+            //change the number in element 0 each time
+            mut_data[0] += 1;
+            println!("In thread {} the value of element 0 is {}", i, mut_data[0]);
         });
     }
 
     thread::sleep(Duration::from_millis(50));
 }
+
+/* 
+ * One of the results of this program is:
+    
+    In thread 2 the value of element 0 is 2
+    In thread 1 the value of element 0 is 3
+    In thread 0 the value of element 0 is 4
+*/
--- memory_profiling/scoped_threads_mem_test/src/main.rs
+++ memory_profiling/scoped_threads_mem_test/src/main.rs
@@ -2,15 +2,48 @@ extern crate crossbeam;
 
 use crossbeam::scope;
 
+fn print_vec(vec: &Vec<i32>) {
+    // the `vec` parameter is part of this scope, so it's owned by `print_vec`
+
+    for i in vec.iter() {
+        println!("{}", i)
+    }
+
+    // now, `vec` is deallocated
+}
+
 fn main() {
-    let mut data = [1, 1, 1];
+    // a vector is not able to be copied by default
+    // so we know that unless we copy or clone it 
+    // explicity that there will only be one.
+    let mut data = vec![1, 1, 1];
 
     crossbeam::scope(|scope| {
         for i in 0..3 {
-            scope.spawn(move || {
-                data[i] += 1;
-                println!("element: {} is {}", i, data[i]);
+            
+            // this line with `move` would be forcing the closure to take 
+            // ownership of its environment
+            //
+            //scope.spawn(move || {
+            scope.spawn(|| {
+                print_vec(&data);
             });
         }
     });
+
+    /* work in progress
+     *
+    // this mutates the values in data but it has to clone them 
+    crossbeam::scope(|scope| {
+        for i in 0..3 {
+
+            // this line with `move` forces the closure to take 
+            // ownership of its environment
+            scope.spawn(move || {
+                data_clone[0] += 1;
+                println!("In thread {} the value of element 0 is {}", i, data_clone[0]);
+            });
+        }
+    });
+    */ 
 }
