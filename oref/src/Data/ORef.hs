ff472fa9bf0a541b0347fab3a4e9bf40850c101f
Michael McGirr <mike@oatsmail.com>
2016-11-13 22:45:57 -0800

    Adding presentation example.

--- oref/src/Data/ORef.hs
+++ oref/src/Data/ORef.hs
@@ -160,3 +160,14 @@ bad = do
     refY <- newORef 5
     readORef refX (writeORef refX)
     readORef refX (\x -> readORef refY (\y -> return (x,y)))
+
+magic :: Int -> Maybe Int
+magic x = evalOwn $ do
+  ref <- newORef x
+  readORef ref (\a -> return (a + 1))
+
+darkMagic :: Int -> Maybe Int
+darkMagic x = evalOwn $ do
+  ref <- newORef x
+  readORef ref (\b -> writeORef ref b)
+  readORef ref (\a -> return (a + 1))
