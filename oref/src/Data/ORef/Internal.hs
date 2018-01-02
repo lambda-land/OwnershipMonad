4d6a518c63254addd8720c41939a9d3bfddf2f50
Mike McGirr <mike@oatsmail.com>
2018-01-01 22:10:04 -0800

    Refactor internals to remove remaining pieces that still use borrow counts.

--- oref/src/Data/ORef/Internal.hs
+++ oref/src/Data/ORef/Internal.hs
@@ -3,29 +3,16 @@ Module: Data.ORef.Internal
 
 Internal types and functions
 -}
-
 module Data.ORef.Internal
   ( ORef(ORef)
   , Own
   , Entry(..)
-  , incBC
-  , decBC
-  , hasBorrowers
-  , hasOtherBorrowers
-  -- , readFlag
-  -- , writeFlag
-  -- , setEntryReadFlag
   , setEntryWriteFlag
   , checkEntryReadFlag
   , checkEntryWriteFlag
-  -- , checkEntry
   , checkORef
-  -- , checkThreadId
   , value
   , getEntry
-  -- , deleteEntry
-  -- , getReadFlag
-  -- , getWriteFlag
   , setReadFlag
   , setWriteFlag
   , getValue
@@ -60,78 +47,39 @@ type Store = IntMap Entry
 
 type Readable = Bool
 type Writeable = Bool
-type BorrowerCount = Int
 
 -- | An entry in the store is
 -- a boolean flag indicating whether this ORef can be read,
 -- a boolean flag indicating whether this ORef can be written to,
--- the number of borrowers
 -- the threadID of the owner,
 -- and a value of arbitrary type.
 data Entry =
-  forall v. Typeable v => Entry Readable Writeable BorrowerCount ThreadId v
-
-
--- | Get the borrower count for an Entry
-borrowerCount :: Entry -> Int
-borrowerCount (Entry _r _w bc _thID _v) = bc
-
--- | Set the borrrower count for an Entry
-setEntryBorrowerCount :: Int -> Entry -> Entry
-setEntryBorrowerCount bc (Entry r w _ thID v) = (Entry r w bc thID v)
-
--- | Increase the borrower count by a certain number
-addToBorrowerCount :: Int -> Entry -> Entry
-addToBorrowerCount newB (Entry r w bc thID v) = (Entry r w (bc + newB) thID v)
-
--- | Decrease the borrower count by a certain number
-reduceBorrowerCount :: Int -> Entry -> Entry
-reduceBorrowerCount lessB (Entry r w bc thID v) = (Entry r w (bc - lessB) thID v)
-
-incBC :: ORef a -> Own ()
-incBC oref = adjustEntry oref (addToBorrowerCount 1)
-
-decBC :: ORef a -> Own ()
-decBC oref = adjustEntry oref (reduceBorrowerCount 1)
-
-
-hasBorrowers :: ORef a -> Own Bool
-hasBorrowers oref = do
-  bc <- getBorrowerCount oref
-  return (bc > 0)
-
--- | hasOtherBorrowers will return if there are other borrowers
--- other than the ORef calling the function - this assumes
--- that hasOtherBorrowers will be used by a borrower
-hasOtherBorrowers :: ORef a -> Own Bool
-hasOtherBorrowers oref = do
-  bc <- getBorrowerCount oref
-  return (bc - 1 > 0)
+  forall v. Typeable v => Entry Readable Writeable ThreadId v
 
 -- | The flag of an entry.
 readFlag :: Entry -> Bool
-readFlag (Entry r _w _ _ _) = r
+readFlag (Entry r _w _ _ ) = r
 
 writeFlag :: Entry -> Bool
-writeFlag (Entry _r w _ _ _) = w
+writeFlag (Entry _r w _ _) = w
 
 -- | Adjust the flag of an Entry to the given flag
 setEntryReadFlag :: Bool -> Entry -> Entry
-setEntryReadFlag b (Entry _ w bc t a) = (Entry b w bc t a)
+setEntryReadFlag b (Entry _ w t a) = (Entry b w t a)
 
 -- | Adjust the flag of an Entry to the given flag
 setEntryWriteFlag :: Bool -> Entry -> Entry
-setEntryWriteFlag b (Entry r _ bc t a) = (Entry r b bc t a)
+setEntryWriteFlag b (Entry r _ t a) = (Entry r b t a)
 
 -- | Check if an Entry can be read and if it is in the same thread
 checkEntryReadFlag :: Entry -> IO Bool
-checkEntryReadFlag (Entry r _w _bc thrId _) = do
+checkEntryReadFlag (Entry r _w thrId _) = do
   threadId <- myThreadId
   return $ (threadId == thrId) && r
 
 -- | Check if an Entry can be written to and if it is in the same thread
 checkEntryWriteFlag :: Entry -> IO Bool
-checkEntryWriteFlag (Entry _r w _bc thrId _) = do
+checkEntryWriteFlag (Entry _r w thrId _) = do
   threadId <- myThreadId
   return $ (threadId == thrId) && w
 
@@ -139,20 +87,18 @@ checkEntryWriteFlag (Entry _r w _bc thrId _) = do
 --
 -- This will also check if the thread ID of the current thread matches the
 -- thread specified in the Entry.
--- and if the Entry has more than 0 borrowers.
 --
 -- Checking the thread is done to prevent a child from using ORef's that it
 -- inherited from its parent but was not explicitely given.
 checkEntry :: Entry -> IO Bool
-checkEntry (Entry r w bc thrId _) = do
+checkEntry (Entry r w thrId _) = do
   threadId <- myThreadId
-  return $ (threadId == thrId) && r && w && (bc <= 0)
+  return $ (threadId == thrId) && r && w
 
 -- | Check if the ORef is able to be read and written to.
 --
 -- This will also check if the thread ID of the current thread matches the
 -- thread specified in the ORef.
--- and if the ORef has more than 0 borrowers.
 --
 -- Checking the thread is done to prevent a child from using ORef's that it
 -- inherited from its parent but was not explicitely given.
@@ -164,7 +110,7 @@ checkORef oref  = do
 
 -- | The value of an entry casted to the expected type.
 value :: Typeable a => Entry -> a
-value (Entry _ _ _ _ v) = case cast v of
+value (Entry _ _ _  v) = case cast v of
     Just a  -> a
     Nothing -> error "internal cast error"
 
@@ -178,10 +124,10 @@ getEntry (ORef i) = get >>= maybe err return . lookup i . snd
   where err = error ("entry not found: " ++ show i)
 
 -- | Set an entry in the store.
-setEntry :: Typeable a => ORef a -> Bool -> Bool -> Int -> a -> Own ()
-setEntry (ORef i) r w bc a = do
+setEntry :: Typeable a => ORef a -> Bool -> Bool -> a -> Own ()
+setEntry (ORef i) r w a = do
   thrId <- liftIO $ myThreadId
-  modifyStore (insert i (Entry r w bc thrId a))
+  modifyStore (insert i (Entry r w thrId a))
 
 -- | Adjust an entry in current store
 --
@@ -200,10 +146,6 @@ getReadFlag oref = fmap readFlag (getEntry oref)
 -- | Get the current flag for writing to an ORef.
 getWriteFlag :: ORef a -> Own Bool
 getWriteFlag oref = fmap writeFlag (getEntry oref)
-
--- | Get the current flag for writing to an ORef.
-getBorrowerCount :: ORef a -> Own Int
-getBorrowerCount oref = fmap borrowerCount (getEntry oref)
 
 -- | Set the read flag for an ORef.
 setReadFlag :: ORef a -> Bool -> Own ()
@@ -224,8 +166,7 @@ setValue :: Typeable a => ORef a -> a -> Own ()
 setValue oref a = do
     r <- getReadFlag oref
     w <- getWriteFlag oref
-    bc <- getBorrowerCount oref
-    setEntry oref r w bc a
+    setEntry oref r w a
 
 
 -- TODO get rid of continueOwn?
